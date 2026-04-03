2af9902adfa5f64ce18b589ace71bbea404aec46
Tanis Pérez Nartallo <tanisperez@gmail.com>
2026-04-03 23:37:49 +0200

    Add null-safety.kts example

---
+++ slides/null-safety.kts
@@ -0,0 +1,11 @@
+#!/usr/bin/env kotlin
+
+val name: String? = null
+
+println(name ?: "valor por defecto")
+println(name?.length)
+println(name?.length ?: 0)
+
+var other: String = "test"
+// other = null // ❌ No compila
+println(other)
