5f916eed10b3e6dab19470ab6e73a1ccefdad128
Tanis Pérez Nartallo <tanisperez@gmail.com>
2026-04-15 23:38:11 +0200

    Change groupId project in demojug

--- demojug/src/main/java/codes/tanis/demojug/DemoJUG.java
+++ demojug/src/main/java/codes/tanis/demojug/DemoJUG.java
@@ -1,4 +1,4 @@
-package com.github.tanisperez.demojug;
+package codes.tanis.demojug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
